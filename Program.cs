65177f330374e219a19a393bbd82860ee13f2f53
Dend1S <78959225+Dend1S@users.noreply.github.com>
2021-02-17 17:05:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            string numer = Console.ReadLine();
+            foreach (char number in numer)
+            {
+                Console.Write(Convert.ToInt32(numer.Contains('5')));
+            }
+        }
+    }
+}
