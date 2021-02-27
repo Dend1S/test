86358aa52f2f74433b0a9d78e6e688e4a3c81e78
Dend1S <78959225+Dend1S@users.noreply.github.com>
2021-02-27 11:12:52 +0200

    Add files via upload

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+
+        enum Muscle
+        {
+            Neck,
+            Shoulders,
+            Chest,
+            Forearms,
+            Abdominals,
+            Biceps,
+            Triceps,
+            Waist,
+            back,
+            Glutes,
+            Hamstring,
+            Calves,
+            Hips,
+          
+        }
+    }
+}
